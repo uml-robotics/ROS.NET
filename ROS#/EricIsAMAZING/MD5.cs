f107fd25af0c39d1b7aa5b120209f877a2facf81
emccann <emccann@cs.uml.edu>
2011-07-15 13:04:44 -0400

    added MD5 class so that I don't need to use "*" for the md5 sum, which would've been a problem for coding up publisher.

---
+++ ROS#/EricIsAMAZING/MD5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace EricIsAMAZING
+{
+    public class MD5
+    {
+        public byte[] Hash;
+        public MD5(byte[] data)
+        {
+            Hash = System.Security.Cryptography.MD5.Create().ComputeHash(data);
+        }
+        public MD5(string data) : this(Encoding.ASCII.GetBytes(data))
+        {
+        }
+        public override string ToString()
+        {
+            if (Hash == null) return "";
+            return (Encoding.ASCII.GetString(Hash).Replace("-",""));
+        }
+    }
+}
