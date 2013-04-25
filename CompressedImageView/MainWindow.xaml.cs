8d4a3e1f998a7fc5b15fee48f32a35705e9ed2e2
DMC413 <chea.alexander@gmail.com>
2013-04-25 12:08:22 -0400

    Pull in alex's guts

--- CompressedImageView/MainWindow.xaml.cs
+++ CompressedImageView/MainWindow.xaml.cs
@@ -25,28 +25,77 @@ using m = Messages.std_msgs;
 using gm = Messages.geometry_msgs;
 using nm = Messages.nav_msgs;
 using sm = Messages.sensor_msgs;
-using System.Text;
+
+using System.Windows.Threading;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+using System.Timers;
 
 #endregion
 
 
 namespace WpfApplication1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public delegate void LoopDelegate();
+        private static System.Timers.Timer aTimer;
+        GamePadState currentState;
+        NodeHandle nh;
+
+        bool end, near;
+        float leftMotor, rightMotor;
+        int hours = 1, minutes = 0, seconds = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            aTimer = new System.Timers.Timer(10000);
+            aTimer.Elapsed += new ElapsedEventHandler(UpdateTimer);
+            aTimer.Interval = 1000;
+        }
+
+        private void UpdateTimer(object source, ElapsedEventArgs e)
+        {
+            if (minutes == 0)
+            {
+                hours--;
+                minutes = 60;
+            }
+
+            if (seconds == 0)
+            {
+                minutes--;
+                seconds = 60;
+            }
+
+            seconds--;
+
+            if ((minutes == 59) && (seconds == 55))
+            {
+                near = true;
+            }
+
+            if ((minutes == 59) && (seconds == 50))
+            {
+                aTimer.Enabled = false;
+                end = true;
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(Timer));
+            Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(Link));
+            Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(Voice));
+
             ROS.ROS_MASTER_URI = "http://10.0.3.88:11311";
-            ROS.ROS_HOSTNAME = "YOURIP";
+            ROS.ROS_HOSTNAME = "10.0.3.101";
             ROS.Init(new string[0], "Image_Test");
+            nh = new NodeHandle();
             new Thread(() =>
             {
                 while (!ROS.shutting_down)
@@ -61,6 +110,120 @@ namespace WpfApplication1
         {
             ROS.shutdown();
             base.OnClosed(e);
+        }
+
+        public void Timer()
+        {
+            TimerTextBlock.Text = "Elapsed: " + hours.ToString("D2") + ':' + minutes.ToString("D2") + ':' + seconds.ToString("D2");
+
+            if (near == true)
+                TimerTextBlock.Foreground = Brushes.Yellow;
+
+            if (end == true)
+            {
+                TimerTextBlock.Foreground = Brushes.Red;
+                TimerStatusTextBlock.Text = "END OF TIME";
+                TimerTextBlock.Text = "Press Right Stick to restart";
+                TimerStatusTextBlock.Foreground = Brushes.Red;
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(Timer));
+        }
+
+        public void Voice()
+        {
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(Voice));
+        }
+
+        public void Link()
+        {
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.IsConnected)
+            {
+                LinkTextBlock.Text = "Link: Connected";
+                LinkTextBlock.Foreground = Brushes.Green;
+
+                // Close Application
+                if ((currentState.Buttons.Back == ButtonState.Pressed) &&
+                    (currentState.Buttons.Start == ButtonState.Pressed) &&
+                    (currentState.Buttons.LeftShoulder == ButtonState.Pressed) &&
+                    (currentState.Buttons.RightShoulder == ButtonState.Pressed)
+                    )
+                {
+                    Close();
+                }
+
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(LeftTriggerButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(RightTriggerButton));
+
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(LeftShoulderButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(RightShoulderButton));
+
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(BackButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(StartButton));
+
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(LeftStickButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(YButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(XButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(BButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(AButton));
+
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(RightStickButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(DPadUpButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(DPadLeftButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(DPadRightButton));
+                Window1.Dispatcher.BeginInvoke(DispatcherPriority.SystemIdle, new LoopDelegate(DPadDownButton));
+            }
+            else if (!currentState.IsConnected)
+            {
+                LinkTextBlock.Text = "Link: Disconnected";
+                LinkTextBlock.Foreground = Brushes.Red;
+            }
+
+            GamePad.SetVibration(PlayerIndex.One, leftMotor, rightMotor);
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(Link));
+        }
+
+        private void MainCameraTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (MainCameraTabControl.SelectedIndex)
+            {
+                case 0:
+                    MainCameraTabControl.Background = Brushes.Gold;
+                    return;
+                case 1:
+                    MainCameraTabControl.Background = Brushes.Blue;
+                    return;
+                case 2:
+                    MainCameraTabControl.Background = Brushes.Red;
+                    return;
+                case 3:
+                    MainCameraTabControl.Background = Brushes.Green;
+                    return;
+            }
+        }
+
+        private void SubCameraTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Create a vertical linear gradient 
+            switch (SubCameraTabControl.SelectedIndex)
+            {
+                case 0:
+                    SubCameraTabControl.Background = Brushes.Gold;
+                    return;
+                case 1:
+                    SubCameraTabControl.Background = Brushes.Blue;
+                    return;
+                case 2:
+                    SubCameraTabControl.Background = Brushes.Red;
+                    return;
+                case 3:
+                    SubCameraTabControl.Background = Brushes.Green;
+                    return;
+            }
         }
     }
 }
---
+++ CompressedImageView/XBOX360Controls.cs
@@ -0,0 +1,346 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.IO;
+using System.Threading;
+using Messages;
+using Messages.custom_msgs;
+using Ros_CSharp;
+using XmlRpc_Wrapper;
+using Int32 = Messages.std_msgs.Int32;
+using String = Messages.std_msgs.String;
+using m = Messages.std_msgs;
+using gm = Messages.geometry_msgs;
+using nm = Messages.nav_msgs;
+using sm = Messages.sensor_msgs;
+
+using System.Windows.Threading;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+using System.Timers;
+
+namespace WpfApplication1
+{
+    public partial class MainWindow : Window
+    {
+        public void LeftTriggerButton()
+        {
+            LeftTrigger.Fill = Brushes.Transparent;
+            LeftTriggerProgressBar.Value = 0;
+            LeftTriggerValueTextBlock.Text = "0%";
+            LeftTriggerValueTextBlock.Margin = new Thickness(101, 0, 0, 219);
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Triggers.Left != 0)
+            {
+                LeftTrigger.Fill = Brushes.White;
+                LeftTriggerProgressBar.Value = currentState.Triggers.Left;
+                LeftTriggerValueTextBlock.Text = (LeftTriggerProgressBar.Value * 100).ToString("F0") + '%';
+                LeftTriggerValueTextBlock.Margin = new Thickness(101, 0, 0, 219 + (currentState.Triggers.Left * 100));
+                leftMotor = currentState.Triggers.Left;
+                GamePad.SetVibration(PlayerIndex.One, leftMotor, rightMotor);
+            }
+            else
+            {
+                leftMotor = 0;
+                GamePad.SetVibration(PlayerIndex.One, leftMotor, rightMotor);
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(LeftTriggerButton));
+        }
+
+        public void RightTriggerButton()
+        {
+            RightTrigger.Fill = Brushes.Transparent;
+            RightTriggerProgressBar.Value = 0;
+            RightTriggerValueTextBlock.Text = "0%";
+            RightTriggerValueTextBlock.Margin = new Thickness(220, 0, 0, 219);
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Triggers.Right != 0)
+            {
+                RightTrigger.Fill = Brushes.White;
+                RightTriggerProgressBar.Value = currentState.Triggers.Right;
+                RightTriggerValueTextBlock.Text = (RightTriggerProgressBar.Value * 100).ToString("F0") + '%';
+                RightTriggerValueTextBlock.Margin = new Thickness(220, 0, 0, 219 + (currentState.Triggers.Right * 100));
+                rightMotor = currentState.Triggers.Right;
+                GamePad.SetVibration(PlayerIndex.One, leftMotor, rightMotor);
+            }
+            else
+            {
+                rightMotor = 0;
+                GamePad.SetVibration(PlayerIndex.One, leftMotor, rightMotor);
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(RightTriggerButton));
+        }
+
+        public void LeftShoulderButton()
+        {
+            LeftShoulder.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.LeftShoulder == ButtonState.Pressed)
+            {
+                LeftShoulder.Fill = Brushes.White;
+
+                if (currentState.Buttons.Y == ButtonState.Pressed)
+                {
+                    MainCamera1.IsSelected = true;
+                }
+
+                if (currentState.Buttons.X == ButtonState.Pressed)
+                {
+                    MainCamera2.IsSelected = true;
+                }
+
+                if (currentState.Buttons.B == ButtonState.Pressed)
+                {
+                    MainCamera3.IsSelected = true;
+                }
+
+                if (currentState.Buttons.A == ButtonState.Pressed)
+                {
+                    MainCamera4.IsSelected = true;
+                }
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(LeftShoulderButton));
+        }
+
+        public void RightShoulderButton()
+        {
+            RightShoulder.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.RightShoulder == ButtonState.Pressed)
+            {
+                RightShoulder.Fill = Brushes.White;
+
+                if (currentState.Buttons.Y == ButtonState.Pressed)
+                {
+                    SubCamera1.IsSelected = true;
+                }
+
+                if (currentState.Buttons.X == ButtonState.Pressed)
+                {
+                    SubCamera2.IsSelected = true;
+                }
+
+                if (currentState.Buttons.B == ButtonState.Pressed)
+                {
+                    SubCamera3.IsSelected = true;
+                }
+
+                if (currentState.Buttons.A == ButtonState.Pressed)
+                {
+                    SubCamera4.IsSelected = true;
+                }
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(RightShoulderButton));
+        }
+
+        public void BackButton()
+        {
+            Back.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.Back == ButtonState.Pressed)
+            {
+                Back.Fill = Brushes.White;
+
+                if (end == false)
+                {
+                    aTimer.Enabled = false;
+                    TimerStatusTextBlock.Text = "Paused";
+                    TimerStatusTextBlock.Foreground = Brushes.Yellow;
+                }
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(BackButton));
+        }
+
+        public void StartButton()
+        {
+            Start.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.Start == ButtonState.Pressed)
+            {
+                Start.Fill = Brushes.White;
+
+                if (end == false)
+                {
+
+                    aTimer.Enabled = true;
+                    TimerStatusTextBlock.Text = "Ticking...";
+                    TimerStatusTextBlock.Foreground = Brushes.Blue;
+                }
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(StartButton));
+        }
+
+        public void LeftStickButton()
+        {
+            LeftStick.Fill = Brushes.Gray;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.LeftStick == ButtonState.Pressed)
+            {
+                LeftStick.Fill = Brushes.DarkGray;
+            }
+
+            LeftStickValue.Margin = new Thickness(52 + (currentState.ThumbSticks.Left.X * 25), 0, 0, 111 + (currentState.ThumbSticks.Left.Y * 25));
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(LeftStickButton));
+        }
+
+        public void YButton()
+        {
+            Y.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.Y == ButtonState.Pressed)
+            {
+                Y.Fill = Brushes.Gold;
+            }
+            else if (currentState.Buttons.Y == ButtonState.Pressed)
+            {
+                Y.Fill = Brushes.Yellow;
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(YButton));
+        }
+
+        public void XButton()
+        {
+            X.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.X == ButtonState.Pressed)
+            {
+                X.Fill = Brushes.Blue;
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(XButton));
+        }
+
+        public void BButton()
+        {
+            B.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.B == ButtonState.Pressed)
+            {
+                B.Fill = Brushes.Red;
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(BButton));
+        }
+
+        public void AButton()
+        {
+            A.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.A == ButtonState.Pressed)
+            {
+                A.Fill = Brushes.Green;
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(AButton));
+        }
+
+        public void RightStickButton()
+        {
+            RightStick.Fill = Brushes.Gray;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.Buttons.RightStick == ButtonState.Pressed)
+            {
+                RightStick.Fill = Brushes.DarkGray;
+
+                if (end == true)
+                {
+                    aTimer.Enabled = false;
+                    hours = 1;
+                    minutes = 0;
+                    seconds = 0;
+                    near = false;
+                    end = false;
+                    TimerStatusTextBlock.Foreground = Brushes.Green;
+                    TimerStatusTextBlock.Text = "Ready";
+                    TimerTextBlock.Foreground = Brushes.Black;
+                }
+            }
+
+            RightStickValue.Margin = new Thickness(238 + (currentState.ThumbSticks.Right.X * 25), 0, 0, 53 + (currentState.ThumbSticks.Right.Y * 25));
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(RightStickButton));
+        }
+
+        public void DPadUpButton()
+        {
+            DPadUp.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.DPad.Up == ButtonState.Pressed)
+            {
+                DPadUp.Fill = Brushes.DarkGray;
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(DPadUpButton));
+        }
+
+        public void DPadLeftButton()
+        {
+            DPadLeft.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.DPad.Left == ButtonState.Pressed)
+            {
+                DPadLeft.Fill = Brushes.DarkGray;
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(DPadLeftButton));
+        }
+
+        public void DPadRightButton()
+        {
+            DPadRight.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.DPad.Right == ButtonState.Pressed)
+            {
+                DPadRight.Fill = Brushes.DarkGray;
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(DPadRightButton));
+        }
+
+        public void DPadDownButton()
+        {
+            DPadDown.Fill = Brushes.Transparent;
+            currentState = GamePad.GetState(PlayerIndex.One);
+
+            if (currentState.DPad.Down == ButtonState.Pressed)
+            {
+                DPadDown.Fill = Brushes.DarkGray;
+            }
+
+            Window1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.SystemIdle, new LoopDelegate(DPadDownButton));
+        }
+    }
+}
