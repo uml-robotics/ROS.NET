1c84687afd0e03a645596a07d4df74ed89773045
gregsjohnson <gsjohnson667@gmail.com>
2013-05-17 12:53:54 -0400

    moved rock counter into user control

--- RockCounterUC/RockCounter.xaml.cs
+++ RockCounterUC/RockCounter.xaml.cs
@@ -1,5 +1,4 @@
-﻿#region Imports
-
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,181 +11,32 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System;
-using System.IO;
-using System.Threading;
-using Messages;
-using Messages.custom_msgs;
-using Ros_CSharp;
-using XmlRpc_Wrapper;
-using Int32 = Messages.std_msgs.Int32;
-using String = Messages.std_msgs.String;
-using m = Messages.std_msgs;
-using gm = Messages.geometry_msgs;
-using nm = Messages.nav_msgs;
-using sm = Messages.sensor_msgs;
-using Messages.arm_status_msgs;
-using Messages.rock_publisher;
-
-// for threading
-using System.Windows.Threading;
 
 // for controller; don't forget to include Microsoft.Xna.Framework in References
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
-// for timer
-using System.Timers;
-
-#endregion
-
-namespace WpfApplication1
+namespace RockCounterUC
 {
-    public partial class MainWindow : Window
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class RockCounter : UserControl
     {
         // controller
         GamePadState currentState;
 
-        // nodes
-        NodeHandle nh;
-
-        // timer near end, timer ended
-        bool end, near;
-
-        // initialize timer values; 1 full hour
-        int hours = 1, minutes = 0, seconds = 0;
-
-        // initialize rock count values to 0
-        int red = 0, orange = 0, yellow = 0, green = 0, blue = 0, purple = 0;
-        
         // ring buffer for rocks, main camera index, sub camera index
         int rockRing = 0, incrementValue;
 
-        Publisher<m.Byte> multiplexPub;
-        Publisher<gm.Twist> velPub;
-
-        DispatcherTimer controllerUpdater;
+        // initialize rock count values to 0
+        int red = 0, orange = 0, yellow = 0, green = 0, blue = 0, purple = 0;
 
         private bool ringIsFree;
 
-        // initialize stuff for MainWindow
-        public MainWindow()
+        public RockCounter()
         {
             InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            controllerUpdater = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 0, 0, 100) };
-            controllerUpdater.Tick += Link;
-            controllerUpdater.Start();
-            
-            // ROS stuff
-            ROS.ROS_MASTER_URI = "http://10.0.3.88:11311";
-            ROS.Init(new string[0], "The_UI");
-            nh = new NodeHandle();
-            armGauge.startListening(nh);
-            battvolt.startListening(nh);
-            EStop.startListening(nh);
-
-            velPub = nh.advertise<gm.Twist>("/cmd_vel", 1);
-            multiplexPub = nh.advertise<m.Byte>("/camera_select", 1);
-
-            new Thread(() =>
-            {
-                while (!ROS.shutting_down)
-                {
-                    ROS.spinOnce(ROS.GlobalNodeHandle);
-                    Thread.Sleep(10);
-                }
-            }).Start();
-
-            SubCamera2.Focus();
-        }
-
-        // close ros when application closes
-        protected override void OnClosed(EventArgs e)
-        {
-            ROS.shutdown();
-            base.OnClosed(e);
-        }
-
-        // timer delegate
-        public void Timer(object dontcare, EventArgs alsodontcare)
-        {
-            // display timer
-            TimerTextBlock.Text = "Elapsed: " + hours.ToString("D2") + ':' + minutes.ToString("D2") + ':' + seconds.ToString("D2");
-
-            // change timer textblock to yellow when near = true;
-            if (near == true)
-                TimerTextBlock.Foreground = Brushes.Yellow;
-
-            // change timer textblock to red when end = true
-            if (end == true)
-            {
-                TimerTextBlock.Foreground = Brushes.Red;
-
-                // display end of time in timer textblock
-                TimerStatusTextBlock.Text = "END OF TIME";
-                // display this in timer status textblock when end is true
-                TimerTextBlock.Text = "Press Right Stick to restart";
-                TimerStatusTextBlock.Foreground = Brushes.Red;
-            }
-        }
-
-        // controller link dispatcher
-        public void Link(object sender, EventArgs dontcare)
-        {
-            // get state of player one
-            currentState = GamePad.GetState(PlayerIndex.One);
-
-            // if controller is connected...
-            if (currentState.IsConnected)
-            {
-                // ...say its connected in the textbloxk, color it green cuz its good to go
-                LinkTextBlock.Text = "Controller: Connected";
-                LinkTextBlock.Foreground = Brushes.Green;
-
-                foreach (Buttons b in Enum.GetValues(typeof(Buttons)))
-                {
-                    Button(b);
-                }
-
-                gm.Twist vel = new gm.Twist { linear = new gm.Vector3 { x = currentState.ThumbSticks.Left.Y }, angular = new gm.Vector3 { z = currentState.ThumbSticks.Left.X } };
-                Console.WriteLine("JOY: (" + vel.linear.x +", " + vel.angular.z+")");
-            }
-            // unless if controller is not connected...
-            else if (!currentState.IsConnected)
-            {
-                // ...have program complain controller is disconnected
-                LinkTextBlock.Text = "Controller: Disconnected";
-                LinkTextBlock.Foreground = Brushes.Red;
-            }
-        }
-
-        private byte maincameramask, secondcameramask;
-
-        // when MainCameraControl tabs are selected
-        private void MainCameraTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            maincameramask = (byte)Math.Round(Math.Pow(2.0, MainCameraTabControl.SelectedIndex));
-            JimCarry();
-        }
-
-        // When SubCameraTabControl tabs are selected
-        private void SubCameraTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            maincameramask = (byte)Math.Round(Math.Pow(2.0, SubCameraTabControl.SelectedIndex));
-            JimCarry();
-        }
-
-
-        //What an odd name to give a function...
-        private void JimCarry()
-        {
-            if (multiplexPub == null) return;
-            m.Byte msg = new m.Byte { data = (byte)(maincameramask | secondcameramask) };
-            multiplexPub.publish(msg);
         }
 
         // stuff that happens when to move arond the ring buffer
@@ -337,80 +187,6 @@ namespace WpfApplication1
             }
         }
 
-        private List<Buttons> knownToBeDown = new List<Buttons>();
-        public void Button(Buttons b)
-        {
-            // if a is pressed
-            if (currentState.IsButtonDown(b))
-            {
-                if (knownToBeDown.Contains(b))
-                    return;
-                knownToBeDown.Add(b);
-                Console.WriteLine("" + b.ToString() + " pressed");
-
-                switch (b)
-                {
-                    case Buttons.Start:
-                        if (end == false)
-                        {
-                            Console.WriteLine("Start timer");
-                            // display that timer is ticking
-                            TimerStatusTextBlock.Text = "Ticking...";
-                            // display in blue
-                            TimerStatusTextBlock.Foreground = Brushes.Blue;
-                        }
-                        break;
-                    case Buttons.Back:
-                        if (end == false)
-                        {
-                            Console.WriteLine("Pause timer");
-                            // display that timer is paused
-                            TimerStatusTextBlock.Text = "Paused";
-                            // display in yellow
-                            TimerStatusTextBlock.Foreground = Brushes.Yellow;
-                        }
-                        break;
-                    case Buttons.DPadDown: DPadDownButton(); break;
-                    case Buttons.DPadUp: DPadUpButton(); break;
-                    case Buttons.DPadLeft: DPadLeftButton(); break;
-                    case Buttons.DPadRight: DPadRightButton(); break;
-                    case Buttons.RightStick: RightStickButton(); break;
-                }
-            }
-            else
-                knownToBeDown.Remove(b);
-        }
-
-        // right stick function; reset timer
-        public void RightStickButton()
-        {
-            // if right stick is clicked/pressed
-            if (currentState.Buttons.RightStick == ButtonState.Pressed)
-            {
-                // if timer is at the end
-                if (end == true)
-                {
-                    Console.WriteLine("Right stick pressed");
-                    // reset hours to one
-                    hours = 1;
-                    // reset minutes to 0
-                    minutes = 0;
-                    // reset seconds to 0
-                    seconds = 0;
-                    // reset near to false
-                    near = false;
-                    // reset end to false
-                    end = false;
-                    // display timer info in green 
-                    TimerStatusTextBlock.Foreground = Brushes.Green;
-                    // display timer info as ready
-                    TimerStatusTextBlock.Text = "Ready";
-                    // display timer in black
-                    TimerTextBlock.Foreground = Brushes.Black;
-                }
-            }
-        }
-        
         // dpad up functions
         public void DPadUpButton()
         {
