c5fba169bbedc48f11f2f0afc4438b02bb6398fd
emccann <emccann@cs.uml.edu>
2013-07-25 14:00:29 -0400

    ROS_MASTER_URI: allow Init to retrieve the uri from an environment variable, and don't exit(1) if the URI is not explicitly set by the program, as it could have been passed in as a remapping

--- ROS_Comm/_Init.cs
+++ ROS_Comm/_Init.cs
@@ -180,35 +180,75 @@ namespace Ros_CSharp
 
         public static void Init(string[] args, string name)
         {
-               Init(args, name, 0);
+            Init(args, name, 0);
         }
 
         public static void Init(string[] args, string name, int options)
         {
-            Console.WriteLine("INIT!");
+            // ROS_MASTER_URI/ROS_HOSTNAME definition precedence:
+            // 1. explicitely set by program
+            // 2. passed in as remap argument
+            // 3. environment variable
+
             IDictionary remapping = new Hashtable();
             for (int i = 0; i < args.Length; i++)
             {
                 if (args[i].Contains(":="))
                 {
                     string[] chunks = args[i].Split(':');
-                    chunks[1].TrimStart('=');
-                    chunks[0].Trim();
-                    chunks[1].Trim();
+                    chunks[1] = chunks[1].TrimStart('=').Trim();
+                    chunks[0] = chunks[0].Trim();
                     remapping.Add(chunks[0], chunks[1]);
+                    switch (chunks[0])
+                    {
+                        //if already defined, then it was defined by the program, so leave it
+                        case "__master": if (string.IsNullOrEmpty(ROS.ROS_MASTER_URI)) ROS.ROS_MASTER_URI = chunks[1].Trim(); break;
+                        case "__hostname": if (string.IsNullOrEmpty(ROS.ROS_HOSTNAME)) ROS.ROS_HOSTNAME = chunks[1].Trim(); break;
+                    }
                 }
             }
+
+            //If ROS.ROS_MASTER_URI was not explicitely set by the program calling Init, and was not passed in as a remapping argument, then try to find it in ENV.
+            if (string.IsNullOrEmpty(ROS.ROS_MASTER_URI))
+            {
+                IDictionary _vars;
+
+                //check user env first, then machine if user doesn't have uri defined.
+                if ((_vars = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.User)).Contains("ROS_MASTER_URI")
+                    || (_vars = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Machine)).Contains("ROS_MASTER_URI"))
+                    ROS.ROS_MASTER_URI = (string)_vars["ROS_MASTER_URI"];
+            }
+
+            //if defined NOW, then add to remapping, or replace remapping (in the case it was explicitly set by program AND was passed as remapping arg)
             if (!string.IsNullOrEmpty(ROS.ROS_MASTER_URI))
             {
-                remapping.Add("__master", ROS.ROS_MASTER_URI);
+                if (remapping.Contains("__master"))
+                    remapping["__master"] = ROS.ROS_MASTER_URI;
+                else
+                    remapping.Add("__master", ROS.ROS_MASTER_URI);
             }
+            else
+                //this is fatal
+                throw new Exception("Unknown ROS_MASTER_URI\n" + @"ROS_MASTER_URI needs to be defined for your program to function.
+Either:
+    set an environment variable called ROS_MASTER_URI,
+    pass a __master remapping argument to your program, 
+    or set the URI explicitely in your program before calling Init.");
+
             if (!string.IsNullOrEmpty(ROS.ROS_HOSTNAME))
             {
-                remapping.Add("__hostname", ROS.ROS_HOSTNAME);
+                if (remapping.Contains("__hostname"))
+                    remapping["__hostname"] = ROS.ROS_MASTER_URI;
+                else
+                    remapping.Add("__hostname", ROS.ROS_MASTER_URI);
             }
+
             if (!string.IsNullOrEmpty(ROS.ROS_IP))
             {
-                remapping.Add("__ip", ROS.ROS_IP);
+                if (remapping.Contains("__ip"))
+                    remapping["__ip"] = ROS.ROS_MASTER_URI;
+                else
+                    remapping.Add("__ip", ROS.ROS_MASTER_URI);
             }
 
             Init(remapping, name, options);
