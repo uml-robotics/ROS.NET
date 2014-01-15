e79db6460ed80a5c71d99138a13b1007c035d93f
emccann <emccann@cs.uml.edu>
2014-01-15 16:44:40 -0500

    Modify WALL-o-IMAGES sample to allow flipping ImageControl's subscribers on and off to debug subscription weirdness

--- CompressedImageView/MainWindow.xaml.cs
+++ CompressedImageView/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using System.Windows.Threading;
 using Messages;
 using Messages.custom_msgs;
 using Ros_CSharp;
+using ROS_ImageWPF;
 using XmlRpc_Wrapper;
 using Int32 = Messages.std_msgs.Int32;
 using String = Messages.std_msgs.String;
@@ -45,32 +46,79 @@ namespace CompressedImageView
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            ROS.ROS_MASTER_URI = "http://robot-lab8:11311";
             ROS.Init(new string[0], "Image_Test");
-            DispatcherTimer testies = new DispatcherTimer() { Interval = new TimeSpan(0,0,0,1) };
-            testies.Tick += new EventHandler((o,e3)=>{
-                gm.Vector3 trans = new gm.Vector3();
-                gm.Quaternion rot = new gm.Quaternion();
-                emTransform wheeee = tf_node.instance.transformFrame("/base_link", "/camera_link", out trans, out rot);
-                if (wheeee != null && wheeee.translation != null)
-                {
-                    Console.WriteLine("base ==> camera: rpy=" + ((180.0 / Math.PI) * wheeee.rotation.getRPY()));
-                }
-                gm.Vector3 trans2 = new gm.Vector3();
-                gm.Quaternion rot2 = new gm.Quaternion();
-                emTransform wheeee2 = tf_node.instance.transformFrame("/camera_link", "/base_link", out trans2, out rot2);
-                if (wheeee2 != null && wheeee2.translation != null)
-                {
-                    Console.WriteLine("camera ==> base: rpy=" + ((180.0 / Math.PI) * wheeee2.rotation.getRPY()));
-                }
-            });
-            testies.Start();
         }
 
         protected override void OnClosed(EventArgs e)
         {
             ROS.shutdown();
             base.OnClosed(e);
+        }
+
+        private string[] topics = new string[6];
+
+        private void flippydippy<T>(int i, T img) where T : UserControl
+        {
+            var c1 = img as ImageControl;
+            var c2 = img as CompressedImageControl;
+            if (c1 != null)
+            {
+                if (topics[i] == null)
+                {
+                    topics[i] = c1.Topic;
+                    c1.guts.fps.Content = "PAUSED";
+                    c1.shutdown();
+                }
+                else
+                {
+                    c1.Topic = topics[i];
+                    c1.guts.fps.Content = "0";
+                    topics[i] = null;
+                }
+            }
+            else if (c2 != null)
+            {
+                if (topics[i] == null)
+                {
+                    topics[i] = c2.Topic;
+                    c2.guts.fps.Content = "PAUSED";
+                    c2.shutdown();
+                }
+                else
+                {
+                    c2.Topic = topics[i];
+                    c2.guts.fps.Content = "0";
+                    topics[i] = null;
+                }
+            }
+            else 
+                Console.WriteLine("TOO MANY ASSUMPTIONS!");
+        }
+
+
+        private void _1(object sender, RoutedEventArgs e)
+        {
+            flippydippy(0, TestImage1);
+        }
+        private void _2(object sender, RoutedEventArgs e)
+        {
+            flippydippy(1, TestImage2);
+        }
+        private void _3(object sender, RoutedEventArgs e)
+        {
+            flippydippy(2, TestImage3);
+        }
+        private void _4(object sender, RoutedEventArgs e)
+        {
+            flippydippy(3, TestImage4);
+        }
+        private void _5(object sender, RoutedEventArgs e)
+        {
+            flippydippy(4, TestImage5);
+        }
+        private void _6(object sender, RoutedEventArgs e)
+        {
+            flippydippy(5, TestImage6);
         }
     }
 }
