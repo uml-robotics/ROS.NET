f5503a308fc971f737fce6c54e0d6b47fc59ec73
emccann <emccann@cs.uml.edu>
2014-11-14 18:00:56 -0500

    WindowScraper: fix dpi/resolution of scraped image, and hopefully make the actual rendering not lock up the UI as hard as it felt like it was preliminarily

--- ROS_ImageUtils/WindowScraper.cs
+++ ROS_ImageUtils/WindowScraper.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using Messages.sensor_msgs;
 using Ros_CSharp;
 
@@ -14,42 +15,38 @@ namespace ROS_ImageWPF
 {
     public class WindowScraper
     {
-        private Timer timer;
+        private DispatcherTimer timer;
         private Publisher<CompressedImage> pub;
         private Window window;
-        private int dpi;
         private int period_ms = Timeout.Infinite;
         private bool running = false;
 
-        public WindowScraper(string topic, Window w, int hz = 1, int dpi = 96) : this(topic, w, TimeSpan.FromSeconds(1.0/((double)hz)), dpi)
+        public WindowScraper(string topic, Window w, int hz = 1) : this(topic, w, TimeSpan.FromSeconds(1.0/((double)hz)))
         {
         }
         
-        public WindowScraper(string topic, Window w, TimeSpan period, int dpi = 96)
+        public WindowScraper(string topic, Window w, TimeSpan period)
         {
             window = w;
-            this.dpi = dpi;
             period_ms = (int) Math.Floor(period.TotalMilliseconds);
-            timer = new Timer(callback, null, Timeout.Infinite, period_ms);
+            timer = new DispatcherTimer(period, DispatcherPriority.Background, callback, window.Dispatcher);
             NodeHandle nh = new NodeHandle();
-            pub = nh.advertise<CompressedImage>(topic, 1, true);
+            pub = nh.advertise<CompressedImage>(topic, 1, false);
         }
 
         public void Start()
         {
-            if (!running)
+            if (!timer.IsEnabled)
             {
-                running = true;
-                timer.Change(0, period_ms);
+                timer.Start();
             }
         }
 
         public void Stop()
         {
-            if (running)
+            if (timer.IsEnabled)
             {
-                timer.Change(Timeout.Infinite, period_ms);
-                running = false;
+                timer.Stop();
             }
         }
 
@@ -59,34 +56,47 @@ namespace ROS_ImageWPF
             pub.shutdown();
         }
 
-        private void callback(object o)
+        private void callback(object o, EventArgs args)
         {
-            window.Dispatcher.BeginInvoke(new Action(() =>
+            double dpiX = -1.0, dpiY = -1.0;
+            CompressedImage cm = new CompressedImage { format = new Messages.std_msgs.String("jpeg"), header = new Messages.std_msgs.Header { stamp = ROS.GetTime() } };
+
+            #region based on
+
+            PresentationSource source = PresentationSource.FromVisual(window);
+
+            if (source != null)
             {
+                dpiX = 96.0*source.CompositionTarget.TransformToDevice.M11;
+                dpiY = 96.0*source.CompositionTarget.TransformToDevice.M22;
+            }
+
+            #endregion
+
+            if (dpiX >= 0 && dpiY >= 0)
+            {
+                
                 #region based on http://blogs.msdn.com/b/saveenr/archive/2008/09/18/wpf-xaml-saving-a-window-or-canvas-as-a-png-bitmap.aspx
 
-                var rtb = new RenderTargetBitmap(
-                    (int) window.Width, //width 
-                    (int) window.Width, //height 
-                    dpi, //dpi x 
-                    dpi, //dpi y 
+                RenderTargetBitmap rtb = new RenderTargetBitmap(
+                    (int) window.ActualWidth, //width 
+                    (int) window.ActualHeight, //height 
+                    dpiX, //dpi x 
+                    dpiY, //dpi y 
                     PixelFormats.Pbgra32 // pixelformat 
                     );
                 rtb.Render(window);
-
-                var enc = new System.Windows.Media.Imaging.JpegBitmapEncoder();
+                JpegBitmapEncoder enc = new System.Windows.Media.Imaging.JpegBitmapEncoder();
                 enc.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(rtb));
-
-                #endregion
-
-                CompressedImage cm = new CompressedImage {format = new Messages.std_msgs.String("jpeg"), header = new Messages.std_msgs.Header {stamp = ROS.GetTime()}};
                 using (MemoryStream ms = new MemoryStream())
                 {
                     enc.Save(ms);
                     cm.data = ms.GetBuffer();
                 }
+                #endregion
+
                 pub.publish(cm);
-            }));
+            }
         }
     }
 }
