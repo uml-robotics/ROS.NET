7f1996bb9fbfaf120da5289732755e60fe487ffc
emccann <emccann@cs.uml.edu>
2014-11-17 12:14:12 -0500

    WindowScraper: handle publication on a thread other than the dispatcher's, and move rendering back to a System.Threading.Timer

--- ROS_ImageUtils/WindowScraper.cs
+++ ROS_ImageUtils/WindowScraper.cs
@@ -15,11 +15,16 @@ namespace ROS_ImageWPF
 {
     public class WindowScraper
     {
-        private DispatcherTimer timer;
+        private Timer timer;
         private Publisher<CompressedImage> pub;
         private Window window;
         private int period_ms = Timeout.Infinite;
         private bool running = false;
+        private Queue<CompressedImage> outboundqueue = new Queue<CompressedImage>();
+        private Thread queueworker;
+        private double dpiX, dpiY;
+        private double WIDTH, HEIGHT;
+        private bool enabled = false;
 
         public WindowScraper(string topic, Window w, int hz = 1) : this(topic, w, TimeSpan.FromSeconds(1.0/((double)hz)))
         {
@@ -28,25 +33,62 @@ namespace ROS_ImageWPF
         public WindowScraper(string topic, Window w, TimeSpan period)
         {
             window = w;
+            window.Dispatcher.Invoke(new Action(() => {
+                w.SizeChanged += new SizeChangedEventHandler(w_SizeChanged);
+                WIDTH = w.RenderSize.Width;
+                HEIGHT = w.RenderSize.Height;
+            }));
             period_ms = (int) Math.Floor(period.TotalMilliseconds);
-            timer = new DispatcherTimer(period, DispatcherPriority.Background, callback, window.Dispatcher);
+            timer = new Timer(callback, null, Timeout.Infinite, period_ms);
             NodeHandle nh = new NodeHandle();
+            if (!topic.EndsWith("/compressed"))
+            {
+                Console.WriteLine("APPENDING /compressed TO TOPIC NAME TO MAKE IMAGE TRANSPORT HAPPY");
+                topic += "/compressed";
+            }
             pub = nh.advertise<CompressedImage>(topic, 1, false);
+            queueworker = new Thread(() =>
+            {
+                Queue<CompressedImage> localqueue = new Queue<CompressedImage>();
+                while (ROS.ok)
+                {
+                    lock (outboundqueue)
+                    {
+                        while (outboundqueue.Count > 0)
+                            localqueue.Enqueue(outboundqueue.Dequeue());
+                    }
+                    CompressedImage cm;
+                    while (localqueue.Count > 0 && (cm = localqueue.Dequeue())!=null)
+                    {
+                        pub.publish(cm);
+                    }
+                    Thread.Sleep(10);
+                }
+            });
+            queueworker.Start();
+        }
+
+        void w_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            WIDTH = e.NewSize.Width;
+            HEIGHT = e.NewSize.Height;
         }
 
         public void Start()
         {
-            if (!timer.IsEnabled)
+            if (!enabled)
             {
-                timer.Start();
+                enabled = true;
+                timer.Change(0, period_ms);
             }
         }
 
         public void Stop()
         {
-            if (timer.IsEnabled)
+            if (enabled)
             {
-                timer.Stop();
+                enabled = false;
+                timer.Change(Timeout.Infinite, period_ms);
             }
         }
 
@@ -54,49 +96,56 @@ namespace ROS_ImageWPF
         {
             Stop();
             pub.shutdown();
+            ROS.shutdown();
+            ROS.waitForShutdown();
+            queueworker.Join();
         }
 
-        private void callback(object o, EventArgs args)
+        private void callback(object o)
         {
-            double dpiX = -1.0, dpiY = -1.0;
+            DateTime start = DateTime.Now;
             CompressedImage cm = new CompressedImage { format = new Messages.std_msgs.String("jpeg"), header = new Messages.std_msgs.Header { stamp = ROS.GetTime() } };
 
-            #region based on
-
-            PresentationSource source = PresentationSource.FromVisual(window);
-
-            if (source != null)
+            if (dpiX <= 0 || dpiY <= 0)
             {
-                dpiX = 96.0*source.CompositionTarget.TransformToDevice.M11;
-                dpiY = 96.0*source.CompositionTarget.TransformToDevice.M22;
+                window.Dispatcher.Invoke(new Action(() =>
+                {
+                    PresentationSource source = PresentationSource.FromVisual(window);
+
+                    if (source != null)
+                    {
+                        dpiX = 96.0*source.CompositionTarget.TransformToDevice.M11;
+                        dpiY = 96.0*source.CompositionTarget.TransformToDevice.M22;
+                    }
+                }));
             }
-
-            #endregion
-
-            if (dpiX >= 0 && dpiY >= 0)
+            
+            window.Dispatcher.Invoke(new Action(() =>
             {
-                
                 #region based on http://blogs.msdn.com/b/saveenr/archive/2008/09/18/wpf-xaml-saving-a-window-or-canvas-as-a-png-bitmap.aspx
-
                 RenderTargetBitmap rtb = new RenderTargetBitmap(
-                    (int) window.ActualWidth, //width 
-                    (int) window.ActualHeight, //height 
+                    (int) WIDTH, //width 
+                    (int) HEIGHT, //height 
                     dpiX, //dpi x 
                     dpiY, //dpi y 
                     PixelFormats.Pbgra32 // pixelformat 
                     );
                 rtb.Render(window);
+
                 JpegBitmapEncoder enc = new System.Windows.Media.Imaging.JpegBitmapEncoder();
                 enc.Frames.Add(System.Windows.Media.Imaging.BitmapFrame.Create(rtb));
+
                 using (MemoryStream ms = new MemoryStream())
                 {
                     enc.Save(ms);
                     cm.data = ms.GetBuffer();
                 }
-                #endregion
 
-                pub.publish(cm);
-            }
+                #endregion
+            }));
+
+            lock (outboundqueue)
+                outboundqueue.Enqueue(cm);
         }
     }
 }
