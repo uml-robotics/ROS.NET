30855db8733182daa7ebd0c7b81d9b4aff29c8e0
emccann <emccann@cs.uml.edu>
2015-09-25 18:13:48 -0400

    CompressedImageView: update API used to toggle each of the 6 image views on and off, and avoid visual studio hosting process (causes visual studio to lock up)

--- CompressedImageView/MainWindow.xaml.cs
+++ CompressedImageView/MainWindow.xaml.cs
@@ -55,40 +55,20 @@ namespace CompressedImageView
             base.OnClosed(e);
         }
 
-        private string[] topics = new string[6];
-
-        private void flippydippy<T>(int i, T img) where T : UserControl
+        private void flippydippy<T>(T img) where T : iROSImage
         {
-            var c1 = img as ImageControl;
-            var c2 = img as CompressedImageControl;
-            if (c1 != null)
+            var i = img as iROSImage;
+            if (i != null)
             {
-                if (topics[i] == null)
+                if (i.IsSubscribed())
                 {
-                    topics[i] = c1.Topic;
-                    c1.guts.fps.Content = "PAUSED";
-                    c1.shutdown();
+                    i.getGenericImage().fps.Content = "PAUSED";
+                    i.Desubscribe();
                 }
                 else
                 {
-                    c1.Topic = topics[i];
-                    c1.guts.fps.Content = "0";
-                    topics[i] = null;
-                }
-            }
-            else if (c2 != null)
-            {
-                if (topics[i] == null)
-                {
-                    topics[i] = c2.Topic;
-                    c2.mGenericImage.fps.Content = "PAUSED";
-                    c2.Desubscribe();
-                }
-                else
-                {
-                    c2.Topic = topics[i];
-                    c2.mGenericImage.fps.Content = "0";
-                    topics[i] = null;
+                    i.getGenericImage().fps.Content = "0";
+                    i.Resubscribe();
                 }
             }
             else 
@@ -98,27 +78,27 @@ namespace CompressedImageView
 
         private void _1(object sender, RoutedEventArgs e)
         {
-            flippydippy(0, TestImage1);
+            flippydippy(TestImage1);
         }
         private void _2(object sender, RoutedEventArgs e)
         {
-            flippydippy(1, TestImage2);
+            flippydippy(TestImage2);
         }
         private void _3(object sender, RoutedEventArgs e)
         {
-            flippydippy(2, TestImage3);
+            flippydippy(TestImage3);
         }
         private void _4(object sender, RoutedEventArgs e)
         {
-            flippydippy(3, TestImage4);
+            flippydippy(TestImage4);
         }
         private void _5(object sender, RoutedEventArgs e)
         {
-            flippydippy(4, TestImage5);
+            flippydippy(TestImage5);
         }
         private void _6(object sender, RoutedEventArgs e)
         {
-            flippydippy(5, TestImage6);
+            flippydippy(TestImage6);
         }
     }
 }
